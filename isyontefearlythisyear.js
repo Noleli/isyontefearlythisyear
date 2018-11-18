a6a82e8a82944e67770763f7e5c62a26db17b8ed
Noah Liebman <noah@noahliebman.net>
2018-11-17 20:53:58 -0600

    using m-d strings

--- isyontefearlythisyear.js
+++ isyontefearlythisyear.js
@@ -12,7 +12,7 @@ function dataCallback(data) {
 function makeDateRange() {
     // pick a gregorian leap year (2016) and use D3 to do the hard work
     let range = d3.utcDay.range(Date.UTC(2016, 0, 1), Date.UTC(2017, 0, 1));
-    return range.map(d => {
-        return { month: d.getUTCMonth() + 1, day: d.getUTCDate() }
-    });
+
+    // scale domains have to be numeric or strings, so using m-d strings (no leading zeros)
+    return range.map(d => (d.getUTCMonth() + 1) + "-" + d.getUTCDate());
 }
