027501e435e7afa77515aa0a0a2e9992089133b0
Noah Liebman <noah@noahliebman.net>
2018-11-17 21:52:08 -0600

    it draws!

--- isyontefearlythisyear.js
+++ isyontefearlythisyear.js
@@ -13,18 +13,41 @@ d3.json("agg_data.json").then(dataCallback);
 let x = d3.scalePoint()
     .domain(makeDateRange());
 
+let histHeight = 60;
+let histY = d3.scaleLinear()
+    .range([histHeight, 0]);
+
+let histLine = d3.line()
+    .x(d => x(d.date))
+    .y(d => histY(d.count));
+
 let aggData, byEvent;
 function dataCallback(data) {
     aggData = data;
-    aggData.forEach(d => d.date = d.month + "-" + d.day);
+    let totalYears = d3.sum(aggData.filter(d => d.event == aggData[0].event), d => d.count);
+
+    aggData.forEach(d => {
+        d.date = d.month + "-" + d.day;
+        d.freq = d.count/totalYears;
+    });
 
     byEvent = d3.nest()
         .key(d => d.event)
         .map(aggData);
+
+    histY.domain([0, d3.max(aggData, d => d.count)]);
+
+    update();
 }
 
 function update() {
+    let events = g.selectAll(".event").data(byEvent.values());
+    events.exit().remove();
+    let eventsEnter = events.enter().append("g").attr("class", "event");
+    eventsEnter.append("path").attr("class", "hist");
+    events = eventsEnter.merge(events);
 
+    events.selectAll("path.hist").attr("d", histLine);
 }
 
 function size() {
@@ -37,7 +60,7 @@ function size() {
     x.range([0, width]);
     svg.attr("width", outerWidth).attr("height", outerHeight);
 
-    update();
+    // update();
 }
 size();
 
